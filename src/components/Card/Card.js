7fccd30e4054d1a7e952046444c5d8ee096772cf
Wallison Henrique <wallisonhen@gmail.com>
2020-10-07 18:11:45 -0300

    Criar componente Card

---
+++ src/components/Card/Card.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Container, Flag, Country, Capital } from './styles';
+import { Link } from 'react-router-dom';
+
+export default function Card ({ country }) {
+	const name = country.nameTranslations[0].value;
+	const { flag, capital, _id } = country;
+
+	return (
+		<Container>
+			<Link to={`/pais/${ _id }`}>
+				<div>
+					<Flag src={ flag.svgFile } alt={` Bandeira ${ name } `} />
+				</div>
+				<div>
+					<Country>{ name }</Country>
+					<Capital>{ capital }</Capital>
+				</div>
+			</Link>
+		</Container>	
+	);
+}
---
+++ src/components/Card/styles.js
@@ -0,0 +1,33 @@
+import styled from 'styled-components';
+
+export const Container = styled.article`
+	flex: 50%;
+	padding: 5px;
+
+	@media (max-width: 767px) {
+		flex: 100%;
+	}
+
+	a {
+		border: 1px solid #ddd;
+		border-radius: 2px;
+		color: #000;
+		display: flex;
+		padding: 10px;
+		text-decoration: none;
+	}
+`;
+
+export const Flag = styled.img`
+	margin-right: 10px;
+	vertical-align: middle;
+	width: 50px;
+`;
+
+export const Country = styled.h2`
+	font-size: 16px;
+`;
+
+export const Capital = styled.p`
+	font-size: 16px;
+`;
