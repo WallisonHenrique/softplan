bd10da998204a55a886de8c3d35887c052245aba
Wallison Henrique <wallisonhen@gmail.com>
2020-10-07 18:24:18 -0300

    Updates

---
+++ precache-manifest.be15f77376ceb916c96415b0cff4ae01.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c77d749b785038a4936ab6d6954502b",
+    "url": "/softplan/index.html"
+  },
+  {
+    "revision": "f89529cde6b1183c057c",
+    "url": "/softplan/static/css/main.76eea258.chunk.css"
+  },
+  {
+    "revision": "0011aa5a8fec7d4dc522",
+    "url": "/softplan/static/js/2.bdd0426d.chunk.js"
+  },
+  {
+    "revision": "c87550333da8df8ab4c80bd1d6e9defb",
+    "url": "/softplan/static/js/2.bdd0426d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f89529cde6b1183c057c",
+    "url": "/softplan/static/js/main.f27472c3.chunk.js"
+  },
+  {
+    "revision": "6410588eccf67a794c58",
+    "url": "/softplan/static/js/runtime-main.22b3e673.js"
+  }
+]);
