54300377c53d9adb05aa53a23655de277e4c5c76
Wallison Henrique <wallisonhen@gmail.com>
2020-10-07 18:08:40 -0300

    Armazenar consulta ao servidor

--- src/App.js
+++ src/App.js
@@ -1,26 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Routes from './routes/Routes';
+
+import { useQuery } from '@apollo/client';
+import { COUNTRIES } from './graphql/queries';
+import { contriesItemsVar } from './graphql';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const { loading, error, data } = useQuery(COUNTRIES);
+	
+	if (loading) return <p>Carregando...</p>;
+  	if (error) return <p>Falha :(</p>;
+  	
+	contriesItemsVar(data.Country);
+
+	return (
+		<Routes />
+	);
 }
 
 export default App;
